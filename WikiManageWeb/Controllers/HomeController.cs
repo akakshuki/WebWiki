646aeb0ec99b3d5573d8b7e0ad7139fcbb497df0
akakshuki <giangbaccai1207@gmail.com>
2019-12-28 02:47:13 +0700

    fix ef service

--- WikiManageWeb/Controllers/HomeController.cs
+++ WikiManageWeb/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WikiManageWeb.ServiceReference1;
 
 namespace WikiManageWeb.Controllers
 {
@@ -11,6 +12,7 @@ namespace WikiManageWeb.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            
             return View();
         }
     }
