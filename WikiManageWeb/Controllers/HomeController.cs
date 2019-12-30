1c61e586a93a494822e8b9e6ccaa1950b484862d
akakshuki <giangbaccai1207@gmail.com>
2019-12-31 01:30:35 +0700

    hava fist layout

--- WikiManageWeb/Controllers/HomeController.cs
+++ WikiManageWeb/Controllers/HomeController.cs
@@ -15,5 +15,7 @@ namespace WikiManageWeb.Controllers
             
             return View();
         }
+
+       
     }
 }
---
+++ WikiManageWeb/Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WikiManageWeb.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult HeaderBar()
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public ActionResult NavigationBar()
+        {
+            return PartialView();
+        }
+    }
+}
